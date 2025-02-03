1d75168015ca521855a471b8098e8814c1ed4d27
Paulo Vanderley <devsgeeknerd@icloud.com>
2025-02-03 12:22:04 -0300

    :sparkles: feat(main): implementar a estrutura inicial da função principal e declarar variáveis para cálculo da área do retângulo.

--- main.c
+++ main.c
@@ -2,4 +2,17 @@
 #include <stdio.h>
 
 // Função principal do programa.
-void main() {}
+void main()
+{
+    // Declara uma variável para armazenar o valor da base do retângulo.
+    float base;
+
+    // Declara uma variável para armazenar o valor da área do retângulo.
+    float area;
+
+    // Declara uma variável para armazenar o valor da altura do retângulo.
+    float altura;
+
+    // Exibe uma mensagem para o usuário digitar o valor da base.
+    printf("Digite o valor da base de um retângulo: ");
+}
